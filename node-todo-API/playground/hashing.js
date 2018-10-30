2ce1a4dee0b37aba415518c9b8c4159ea0542325
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-10-30 17:18:40 +0530

    sign up with tokens methods

---
+++ node-todo-API/playground/hashing.js
@@ -0,0 +1,53 @@
+const {SHA256} = require('crypto-js');
+// SHA-256 Cryptographic Hash Algorithm. A cryptographic hash 
+// (sometimes called 'digest') is a kind of 'signature' for a text 
+// or a data file. SHA-256 generates an almost-unique 256-bit (32-byte)
+//  signature for a text.
+
+const jwt = require('jsonwebtoken');
+
+var data = {
+    id: 10
+};
+
+var token = jwt.sign(data, '123abc');
+console.log(`Token : ${token}`);
+
+var decoded = jwt.verify(token,'123abc');
+
+console.log("Decoded: ",decoded);
+
+
+
+/*var message = 'I am user number 3';
+
+var hash = SHA256(message).toString();
+
+console.log(`Message : ${message}`);
+console.log(`Hash: ${hash}`);
+
+var data = {
+    id: 4
+};
+
+var token = {
+    data,
+    hash: SHA256(JSON.stringify(data) + 'something').toString()
+};
+
+token.data.id = 5;
+token.hash = SHA256(JSON.stringify(token.data)).toString();
+
+var resultHash = SHA256(JSON.stringify(token.data) + 'something').toString();
+
+if(resultHash === token.hash){
+    console.log('Data was not changed');
+}else{
+    console.log('Data was change......Carefull');
+    
+}
+*/
+
+
+
+
